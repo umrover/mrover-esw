6c1ba381feb2c8a7f8faa4d4007a6606580d25dc
Artyom (Art) Boyarov <artyom.boyarov@gmail.com>
2024-10-10 17:04:23 -0400

    Added CAN transmitter stm32

--- src/canalyzer/Core/Inc/main.h
+++ src/canalyzer/Core/Inc/main.h
@@ -53,7 +53,7 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-
+void H_EXTI_13(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
