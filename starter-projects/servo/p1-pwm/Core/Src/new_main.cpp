d88c9956519748e8be63a9bf8959c96e30b1dc75
snovyang <snovyang@umich.edu>
2024-10-31 20:01:50 -0400

    starter_project

--- starter-projects/servo/p1-pwm/Core/Src/new_main.cpp
+++ starter-projects/servo/p1-pwm/Core/Src/new_main.cpp
@@ -5,8 +5,19 @@ extern TIM_HandleTypeDef htim1;
 
 void new_main(){
 
+	Servo servo(&htim1,TIM_CHANNEL_1);
+	servo.start_servo();
+
 
   while(1){
+	  servo.set_servo_angle(0);
+	  HAL_Delay(1000);
+	  servo.set_servo_angle(90);
+	  HAL_Delay(1000);
+	  servo.set_servo_angle(0);
+	  HAL_Delay(1000);
+	  servo.set_servo_angle(180);
+	  	  HAL_Delay(1000);
 
   }
 }
--- starter-projects/servo/p1-pwm/Core/Src/servo.cpp
+++ starter-projects/servo/p1-pwm/Core/Src/servo.cpp
@@ -2,15 +2,17 @@
 
 // TODO: Implement the Servo constructor
 Servo::Servo(TIM_HandleTypeDef* tim_handle, uint32_t channel) {
-
+	m_tim_handle = tim_handle;
+	m_channel = channel;
 }
 
 // TODO: Start the PWM signal generation
 void Servo::start_servo() {
-
+	HAL_TIM_PWM_Start(this->m_tim_handle, this->m_channel);
 }
 
 // TODO: Move the servo to the specified angle
 void Servo::set_servo_angle(int angle) {
-
+	uint32_t ticks = (((angle + 90) /180) * 1000) + 999;
+	__HAL_TIM_SET_COMPARE(this->m_tim_handle,this->m_channel, ticks);
 }
