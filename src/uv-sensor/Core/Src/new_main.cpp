70cc745275734cde67c103a3ed41f054bb1d8782
nbaskins <nbaskins@NatesLaptop>
2024-11-09 16:50:27 -0500

    Added MethaneSensor class that uses the ADCSensor class. Did the math to get the ppm conversion for methane concentration but has not been tested.

--- src/uv-sensor/Core/Src/new_main.cpp
+++ src/uv-sensor/Core/Src/new_main.cpp
@@ -1,20 +1,20 @@
 #include "new_main.h"
 #include "hardware_adc.hpp"
 #include "UVSensor.hpp"
+#include "MethaneSensor.hpp"
 
 extern ADC_HandleTypeDef hadc1;
 
 mrover::ADCSensor adc_sensor = mrover::ADCSensor(&hadc1, 1);
-mrover::UVSensor uv_sensor = mrover::UVSensor(&adc_sensor, 0);
+mrover::MethaneSensor methane_sensor = mrover::MethaneSensor(&adc_sensor, 0);
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
-	uv_sensor.update_uv_async();
+	methane_sensor.update_ppm_async();
 }
 
 void new_main(){
 	float x = 0;
-	adc_sensor.update();
 	while(1){
-		x = uv_sensor.get_current_uv();
+		x = methane_sensor.update_ppm_blocking();
 	}
 }
