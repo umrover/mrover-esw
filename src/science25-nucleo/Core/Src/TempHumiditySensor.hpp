7a5d0c6a9b3c549a1cb291cd1526711e7e4a8e9a
Anthony Varkey <anthonyvarkey@gmail.com>
2024-11-24 14:13:14 -0500

    Revised classes + O2 calibrates

--- src/science25-nucleo/Core/Src/TempHumiditySensor.hpp
+++ src/science25-nucleo/Core/Src/TempHumiditySensor.hpp
@@ -1,21 +1,22 @@
 #include "main.h"
 
+#define TEMP_HUM_ADDRESS 0x38
+
 namespace mrover {
 	class TempHumiditySensor  {
 	private:
 		float temp;
 		float humidity;
 		uint8_t dev_addr;
-		uint8_t* buf;
-		uint16_t size;
 		I2C_HandleTypeDef *i2c;
 
 	public:
-		TempHumiditySensor (uint8_t addr_in, uint8_t* buf_in, uint16_t size_in, I2C_HandleTypeDef *i2c_in)
-			: temp(0), humidity(0), dev_addr(addr_in), buf(buf_in), size(size_in), i2c(i2c_in){
+		TempHumiditySensor (I2C_HandleTypeDef *i2c_in)
+			: temp(0), humidity(0), dev_addr(TEMP_HUM_ADDRESS), i2c(i2c_in){
 		};
 
 		void update_temp_humidity() {
+			uint8_t buf[6];
 			HAL_StatusTypeDef status;
 			uint8_t command[3] = {0xAC, 0x33, 0x00};
 			status = HAL_I2C_Master_Transmit(i2c, (dev_addr << 1), command, 3, 100);
@@ -25,7 +26,7 @@ namespace mrover {
 
 			HAL_Delay(100);
 
-			status = HAL_I2C_Master_Receive(i2c, (dev_addr << 1) | 0x01, buf, size, 100);
+			status = HAL_I2C_Master_Receive(i2c, (dev_addr << 1) | 0x01, buf, 6, 100);
 			if (status != HAL_OK){
 				Error_Handler();
 			}
--- src/science25-nucleo/Core/Src/new_main.cpp
+++ src/science25-nucleo/Core/Src/new_main.cpp
@@ -9,17 +9,13 @@ extern ADC_HandleTypeDef hadc1;
 extern I2C_HandleTypeDef hi2c1;
 extern I2C_HandleTypeDef hi2c2;
 
-#define ADDRESS_3 0x73
-#define OXYGEN_DATA_REGISTER 0x03
-#define OXYGEN_DATA_LENGTH 8
-#define TEMP_HUM_ADDRESS 0x38
-#define TEMP_HUM_SIZE 6
-uint8_t buf[3];
-uint8_t buf_two[6];
+
+
+
 mrover::ADCSensor adc_sensor = mrover::ADCSensor(&hadc1, 0);
 mrover::MethaneSensor methane_sensor = mrover::MethaneSensor(&adc_sensor, 0);
-mrover::OxygenSensor oxygen_sensor = mrover::OxygenSensor(ADDRESS_3, OXYGEN_DATA_REGISTER, buf, OXYGEN_DATA_LENGTH, &hi2c2);
-mrover::TempHumiditySensor th_sensor = mrover::TempHumiditySensor(TEMP_HUM_ADDRESS, buf_two, TEMP_HUM_SIZE, &hi2c2);
+mrover::OxygenSensor oxygen_sensor = mrover::OxygenSensor(&hi2c2);
+mrover::TempHumiditySensor th_sensor = mrover::TempHumiditySensor(&hi2c2);
 
 //void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
 //
@@ -27,9 +23,8 @@ mrover::TempHumiditySensor th_sensor = mrover::TempHumiditySensor(TEMP_HUM_ADDRE
 
 void new_main(){
 	float x = 0;
+	float m = oxygen_sensor.calibrate_oxygen();
 	while(1){
-		th_sensor.update_temp_humidity();
-		x = th_sensor.get_current_temp();
-		x = th_sensor.get_current_humidity();
+		x = oxygen_sensor.update_oxygen();
 	}
 }
