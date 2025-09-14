8b2ff34c05f4abe5bfebd9672dd940fefb88216f
wilsonchen02 <wilchen@umich.edu>
2025-09-14 15:15:48 -0400

    Set up line parsing for message and signal

---
+++ dbc/dbc_parser.py
@@ -0,0 +1,65 @@
+import os
+import re
+
+# Global variables
+
+# TODO: add some type hints
+
+#
+def parse_message(line):
+    id = line[1]
+    msg_name = line[2][:-1]
+    msg_byte_length = line[3]
+    sender = line[4]
+
+    # print(line)
+    # print(id)
+    # print(msg_name)
+    # print(msg_byte_length)
+    # print(sender)
+
+#
+def parse_signal(line):
+    # print(line)
+    sg_name = line[1]
+
+    bits_section = re.split(r'[|@]', line[3])    # I'm sorry
+    bit_start = bits_section[0]
+    bit_length = bits_section[1]
+    isLittleEndian = bool(bits_section[2][0])
+    isSigned = True if bits_section[2][1] == "-" else False
+
+    scale_offset_section = line[4][1:-1]
+    scale_offset_section = scale_offset_section.split(",")
+    scale = scale_offset_section[0]
+    offset = scale_offset_section[1]
+
+    min_max_section = line[5][1:-1]
+    min_max_section = min_max_section.split("|")
+    val_min = min_max_section[0]
+    val_max = min_max_section[1]
+
+    unit = line[6][1:-1]
+    receiver = line[7]
+    
+
+#
+def parse_file(file_handle):
+    with open(file_handle) as file:
+        for line in file:
+            trimmed_line = line.strip()
+            split_line = trimmed_line.split(" ")
+            if split_line[0] == "BO_":
+                parse_message(split_line)
+            elif split_line[0] == "SG_":
+                parse_signal(split_line)
+
+# Get path of current script
+path = os.path.dirname(os.path.realpath(__file__))
+# Look through directory for .dbc files
+for root, dirs, files in os.walk(path):
+    for file in files:
+        # Open the .dbc file
+        if file.endswith(".dbc"):
+            # Helper function to parse .dbc file
+            parse_file(file)
