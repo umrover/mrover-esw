8b4473f4c7f80953aa193778d0bbf67eaf05b026
matthew <mattsasato@outlook.com>
2025-10-07 20:50:08 -0400

    logger is ok, yaml parsing started

--- src/canalyzer/logger.cpp
+++ src/canalyzer/logger.cpp
@@ -11,6 +11,7 @@
 #include <linux/can/raw.h>
 #include <queue>
 #include <chrono>
+#include "Yaml.hpp"
 
 std::string decode(canid_t can_id);
 
@@ -36,7 +37,9 @@ class Logger {
         std::string can_id;
         influxdb_cpp::server_info si;
         std::unordered_set<int> valid_ids;
+        std::unordered_set<int> listen_ids;
         std::queue<canfd_frame> buffer;
+        bool log_all = false;
 
 
 
@@ -48,14 +51,20 @@ class Logger {
 
             if (ioctl(bus_socket, SIOGIFINDEX, &ifr) < 0) {
                 std::cerr << "ioctl broke" << std::endl;
-                exit(1);
+                //blow up exit(1)?
             }
             addr.can_family = AF_CAN;
             addr.can_ifindex = ifr.ifr_ifindex;
 
             if (bind(bus_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
                 std::cerr << "bind bus_socket broke" << std::endl;
-                exit(1);
+                //blow up exit(1)?
+            }
+
+            int enable_canfd = 1;
+            if (setsockopt(bus_socket, SOL_CAN_RAW, CAN_RAW_FD_FRAMES, &enable_canfd, sizeof(enable_canfd)) != 0) {
+                std::cerr << "socket did not set to CAN_RAW_FD_FRAMES" << std::endl;
+                //blow up exit(1)?
             }
         }
 
@@ -71,28 +80,32 @@ class Logger {
         void start() {
             init_bus();
             struct canfd_frame cfd;
-            while (true) {
+            while (true) {                                                              //catch an interupt instead?
+
                 //read a vcan message from bus (can_id)
                 //insert into buffer
                 //call to decode
                 //push to influx and logger (asci)
                 //done
-                ssize_t bytes_read = read(bus_socket, &cfd, CANFD_MTU); //CANFD_MTU is macro for sizeof(canfd_frame)
-                if (bytes_read != CANFD_MTU) {
-                    std::cerr << "read a can frame of different size than canfd, expected: " 
-                    << CANFD_MTU << " got: " << bytes_read << std::endl;
-                    exit(1);
+
+                ssize_t bytes_read;
+                while (bytes_read = read(bus_socket, &cfd, CANFD_MTU)) {                //CANFD_MTU is macro for sizeof(canfd_frame)
+                    if (bytes_read != CANFD_MTU) {
+                        std::cerr << "read a can frame of different size than canfd, expected: " 
+                        << CANFD_MTU << " got: " << bytes_read << std::endl;
+                        exit(1);
+                    }
+                    if (valid_ids.find(cfd.can_id) == valid_ids.end()) {
+                        std::cerr << "read a canid that is not part of the valid set" << std::endl;
+                        //probably should log here
+                    }
+                    buffer.emplace(cfd);
                 }
-                if (valid_ids.find(cfd.can_id) == valid_ids.end()) {
-                    std::cerr << "read a canid that is not part of the valid set" << std::endl;
-                    //exit(1)?
-                }
-                buffer.emplace(cfd);
                 cfd = buffer.front();
                 std::string resp;
 
-                //calling decode
-                std::string decoded_message = decode(cfd.can_id);
+                
+                std::string decoded_message = decode(cfd.can_id);                      //calling decode
                 long long ts = now_ns();
                 
                 int ret = influxdb_cpp::builder()
@@ -106,3 +119,23 @@ class Logger {
         }
 
 };
+
+void logger_factory(std::vector<Logger> loggers, std::string path) {
+    //will init a vector of configured Loggers from a yaml found at path
+
+    int size;
+    Yaml::Node root;
+    try {
+        Yaml::Parse(root, path);
+    } catch (const Yaml::Exception e) {
+        std::cerr << "cautch YAML error while parsing: " << e.what() << std::endl;
+    }
+    size = root["logger_bus_size"].As<int>();
+    if (size > 4) {
+        std::cerr << "cannot support more than 4 can busses" << std::endl;
+    }
+    loggers.reserve(size);   
+    for (int i = 0; i < size; ++i) {
+        
+    }
+}
