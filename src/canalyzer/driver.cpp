eab3822cff2c0803664668ee8732bb6ed4951091
matthew <mattsasato@outlook.com>
2025-10-19 14:44:58 -0400

    yaml parse kinda work

--- src/canalyzer/driver.cpp
+++ src/canalyzer/driver.cpp
@@ -3,5 +3,8 @@
 
 int main() {
     std::vector<Logger> loggers;
-    logger_factory(loggers);
+    logger_factory(loggers, "logger_start.yaml");
+    for (size_t i = 0; i < loggers.size(); ++i) {
+        loggers[i].print(std::cout);
+    }
 }
