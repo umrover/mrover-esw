cfaf6d9873b3b2c259a9009be1ce963413404e45
monjacob <monjacob@umich.edu>
2025-10-23 21:11:42 -0400

    Jacob starter projects

--- starter-projects/servo/p1-pwm/Core/Src/servo.cpp
+++ starter-projects/servo/p1-pwm/Core/Src/servo.cpp
@@ -2,15 +2,22 @@
 
 // TODO: Implement the Servo constructor
 Servo::Servo(TIM_HandleTypeDef* tim_handle, uint32_t channel) {
-
+	m_tim_handle = tim_handle;
+	m_channel = channel;
 }
 
 // TODO: Start the PWM signal generation
 void Servo::start_servo() {
-
+	HAL_TIM_PWM_Start(m_tim_handle, m_channel);
 }
 
 // TODO: Move the servo to the specified angle
 void Servo::set_servo_angle(int angle) {
-
+	if (angle == 90) {
+		__HAL_TIM_SET_COMPARE(m_tim_handle, m_channel, 2000);
+	} else if (angle == -90) {
+		__HAL_TIM_SET_COMPARE(m_tim_handle, m_channel, 1000);
+	} else {
+		__HAL_TIM_SET_COMPARE(m_tim_handle, m_channel, 1500);
+	}
 }
--- starter-projects/temp-humidity/Core/Src/th_sensor.cpp
+++ starter-projects/temp-humidity/Core/Src/th_sensor.cpp
@@ -3,7 +3,7 @@
 #include "main.h"
 
 // TODO: add sensor I2C address
-#define TEMP_HUM_ADDRESS 
+#define TEMP_HUM_ADDRESS 0x38
 
 extern I2C_HandleTypeDef hi2c3;
 
@@ -13,12 +13,20 @@ I2C_HandleTypeDef* i2c;
 uint8_t th_buf[6];
 double temp;
 double humidity;
+uint32_t raw_temp;
+uint32_t raw_humidity;
 
 void eventLoop() {
 	// TODO: implement main loop
+	HAL_Delay(100);
 	while (true) {
-		
+		th_buf[0] = 0xAC;
+		th_buf[1] = 0x33;
+		th_buf[2] = 0x00;
+		HAL_I2C_Master_Transmit_IT(i2c, TEMP_HUM_ADDRESS << 1, th_buf, 3);
+		HAL_Delay(1000);
 	}
+
 }
 
 void init() {
@@ -36,12 +44,16 @@ extern "C" {
 
 	// TODO: implement transmit callback
 	void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef* hi2c) {
-		
+		HAL_I2C_Master_Receive_IT(mrover::i2c, (TEMP_HUM_ADDRESS << 1) | 1, mrover::th_buf, 6);
+
 	}
 
 	// TODO: implement receive callback
 	void HAL_I2C_MasterRxCpltCallback (I2C_HandleTypeDef* hi2c) {
-		
+		mrover::raw_humidity = (mrover::th_buf[1] << 12) + (mrover::th_buf[2] << 4) + (mrover::th_buf[3] >> 4);
+		mrover::raw_temp = ((mrover::th_buf[3] & 0x0F) << 16) + (mrover::th_buf[4] << 8) + mrover::th_buf[5];
+		mrover::humidity = ((double)mrover::raw_humidity / std::pow(2, 20)) * 100;
+		mrover::temp = (((double)mrover::raw_temp / std::pow(2, 20)) * 200) - 50;
 	}
 
 }
