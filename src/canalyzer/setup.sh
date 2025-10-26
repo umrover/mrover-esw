b175628da9f325f5f06cc0146098265cb2976ba5
matthew <mattsasato@outlook.com>
2025-10-26 14:07:44 -0400

    docker compose kinda works, maybe need to fix ioctl, vcan, and bind

--- src/canalyzer/setup.sh
+++ src/canalyzer/setup.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
+directoryExists()
+cleanup()
+docker-compose up --build 2>&1 | tee -a docker_compose.log
+while ($run == 1) {
+    check_docker()
+    trap cleanup SIGINT
+}
 
-docker-compose up --build
 
-directoryExists($1, $2) {
+
+
+directoryExists() {
     if [ -d "/grafana_data" ]; then
         echo "grafana_data exists"
+        
+        user=$(stat -c "%U" "/grafana_data")
+        if [ "$user" != "472" ]; then
+            echo "Adjusting permissions for grafana_data..."
+            chown 472:472 "/grafana_data"
+        else
+            echo "Ownership already correct (472)"
+        fi
     else 
-        echo "grafana_data does not exist, creating and setting permissions"
+        echo "grafana_data does not exist, creating and setting permissions..."
         mkdir "/grafana_data"
-        chown 472:472 grafana_data
+        chown 472:472 "/grafana_data"
+    fi
+
+    if [ -d "/influxdb_data" ]; then
+        echo "influxdb_data exists"
+        
+        user=$(stat -c "%U" "/influxdb_data")
+        if [ "$user" != "1500   " ]; then
+            echo "Adjusting permissions for influxdb_data..."
+            chown 1500:1500 "/influxdb_data"
+        else
+            echo "Ownership already correct (1500)"
+        fi
+    else 
+        echo "influxdb_data does not exist, creating and setting permissions..."
+        mkdir "/influxdb_data"
+        chown 1500:1500 "/influxdb_data"
     fi
 }
 
-influxdbExists() {
-
+deadContainers() {
+    
 }
+
+
 
 
 #check for dead containers
@@ -31,14 +65,25 @@ influxdbExists() {
 #       /var/lib/influxdb/meta
 #
 
+#influx is uid 1500
+
 
 #on kill the shell should gracefully exit by closing all docker containers and saving data
+
+cleanup() {
+    echo "Gracefully exiting: "
+    docker-compose down
+    exit 0
+}
 
 
 #vibecoded snippet to log docker, need to change maybe
 
-while true; do
-  echo "---- $(date) ----" >> docker_status.log
-  docker ps -a >> docker_status.log
-  sleep 60  # check every minute
-done
+check_docker() {
+    while true; do
+    echo "---- $(date) ----" >> docker_status.log
+    docker ps -a >> docker_status.log
+    sleep 60  # check every minute
+    trap cleanup SIGINT
+    done
+}
