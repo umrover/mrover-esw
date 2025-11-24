3161b04dcb2a79cc0d3057372b5f376839fea20c
monjacob <monjacob@umich.edu>
2025-11-23 22:10:06 -0500

    flash read/write progress

--- lib/stm32/Inc/hw/flash.hpp
+++ lib/stm32/Inc/hw/flash.hpp
@@ -2,12 +2,17 @@
 #include <cstdint>
 #include <cstring>
 #include <cstdio>
+#include <array>
 
 // suggestion flash interface based on the mjbots library
 // https://github.com/mjbots/mjlib/blob/master/mjlib/micro/flash.h
 namespace mrover {
 
-  <template typename Cfg>
+  constexpr uint32_t LAST_PAGE_START = 0x0801F800;
+  constexpr uint32_t LAST_PAGE_END = 0x0801FFFF;
+  constexpr uint32_t PAGE_SIZE = 2048;
+  constexpr int CHUNK_SIZE = 0x08;
+
 
   class Flash {
   private:
@@ -21,20 +26,22 @@ namespace mrover {
 
   public:
     Flash() = default;
-    Flash(const Cfg &config_in) : config(config_in) {};
     ~Flash() = default;
+    Flash (uint32_t start_addr) {
+      info.start = start_addr;
+      info.end = LAST_PAGE_END;
+    };
    
     struct Info {
-      char* start = nullptr;
-      char* end = nullptr;
+      uint32_t start = 0;
+      uint32_t end = 0;
     };
 
     // LAST PAGE OF FLASH ONLY
     Info info {
-      info.start = (char*)0x0801F800,
-      info.end = (char*)0x0801FFFF
+      info.start = 0x0801F800,
+      info.end = 0x0801FFFF
     };
-    Cfg config;
 
     /// Erase the entirety of the managed flash section.
     void erase() {
@@ -69,7 +76,9 @@ namespace mrover {
       lock();
 
       if (status != HAL_OK) {
-        printf("WRITE FAILED");
+        printf("WRITE FAILED\n\r");
+      } else {
+        printf("WRITE SUCCESS\n\r");
       }
     }
 
@@ -96,12 +105,100 @@ namespace mrover {
       // printf("READING ADDRESS 0x%lx, READ VALUE: %lu\n\r", start_addr, word);
       return word;
     } 
+    auto read_byte(uint32_t start_addr) -> uint8_t {
+      uint8_t byte = *(__IO uint8_t *)start_addr;
+      return byte;
+    }
 
     // Calculate what page of flash an address is in.
     auto GetPage(uint32_t address) -> uint32_t  {
-      return (address - 0x08000000) / 2048;
+      return (address - 0x08000000) / PAGE_SIZE;
     }
 
+    // Calculate start address of page page_num
+    auto get_page_start(uint32_t page_num) -> uint32_t {
+      return 0x08000000 + (page_num * PAGE_SIZE);
+    }
+
+    // get physical address from custom address
+    auto get_physical_addr(uint32_t address) -> uint32_t {
+      return info.start + address;
+    }
+    
+    // get start address of double word custom address is located in
+    auto get_double_word_start(uint32_t address) -> uint32_t {
+      uint32_t physical_addr = get_physical_addr(address);
+      return physical_addr = physical_addr & ~0x7ULL;
+    }
+
+    // writes a value in the correct double word, given its size
+    template <typename T>
+    void write(uint32_t address, size_t size_of, T value) {  
+      uint32_t dbl_word_start = get_double_word_start(address);
+      int address_offset = address % 8;
+
+      uint64_t existing = read_double_word(dbl_word_start);
+
+      uint64_t mask = 0;
+      for (size_t i = 0; i < size_of; ++i) {
+        mask |= 0xFFULL << ((address_offset + i) * 8);
+      }
+      
+      uint64_t cleared = existing & ~mask;
+      uint64_t new_data = 0;
+      memcpy(&new_data, &value, size_of);
+      new_data <<= (address_offset * 8);
+
+      new_data |= cleared;
+      program_double_word(dbl_word_start, new_data);
+    }
+
+    template <typename T>
+    T read(uint32_t address) {
+      uint32_t dbl_word_start = get_double_word_start(address);
+      int address_offset = address % 8;
+      uint32_t size_of = sizeof(T);
+      
+      uint64_t dbl_word = read_double_word(dbl_word_start);
+
+      uint64_t shifted = dbl_word >> (address_offset * 8);
+
+      uint64_t mask = 0;
+      for (size_t i = 0; i < size_of; ++i) {
+        mask |= 0xFF << (i * 8);
+      }
+      shifted &= mask;
+
+      T result;
+      memcpy(&result, &shifted, size_of);
+      return result;
+
+    }
+  
+    // copy a 2KB page of flash into array
+    void copy_page(std::array<uint8_t, PAGE_SIZE> &buffer, uint32_t page_num) {
+      uint32_t curr_addr = get_page_start(page_num);
+      for (int i = 0; i < PAGE_SIZE; ++i) {
+        uint8_t byte = read_byte(curr_addr);
+        buffer[i] = byte;
+      }
+    }
+
+    // once changes are finalized, erase and rewrite a flash page
+    void flush(std::array<uint8_t, PAGE_SIZE> &buffer, uint32_t page_num) {
+      uint32_t curr_addr = get_page_start(page_num);
+      erase();
+      for (int i = 0; i < PAGE_SIZE / 8; ++i) {
+        uint64_t double_word = 0;
+        uint32_t first = i * 8;
+        for (int i = 0; i < 8; ++i) {
+          double_word |= (buffer[first + i] << (8 * i));
+        }
+        program_double_word(curr_addr, double_word);
+        curr_addr += 8;
+      }
+    }
+  
   };
 
 }
--- src/flash-devel/Core/Src/flash_test.cpp
+++ src/flash-devel/Core/Src/flash_test.cpp
@@ -2,6 +2,7 @@
 #include "hw/flash.hpp"
 #include "stm32g4xx_hal_gpio.h"
 #include <cstdio>
+#include <variant>
 
 extern "C" {
     #include <stdio.h>
@@ -9,7 +10,7 @@ extern "C" {
 
 namespace mrover {
 
-  Flash flash;
+  Flash flash(0x0801F800);
   int count;
   uint32_t start_addr;
   uint32_t curr_addr;
@@ -17,13 +18,22 @@ namespace mrover {
   float pi = 3.14159;
   float read_float = 0.0;
   auto init() -> void {
+    printf("===== RESET =====\n\r");
     count = 1;
     start_addr = (uint32_t)flash.info.start;
     // printf("Init start_addr: %lx\n\r", start_addr);
     curr_addr = start_addr;
     // printf("Init curr_addr: %lx\n\r", curr_addr);
-    printf("PI is : %f\n\r", pi);
+    
     flash.erase();
+    uint64_t combined =  0;
+    combined |= ((uint64_t)8 << 56) | ((uint64_t)8 << 48) | ((uint64_t)16 << 32) | ((uint64_t)32 << 0);
+    flash.write(0x00, 8, combined);
+    flash.write(0x08, 2, (uint16_t)(16));
+    flash.write(0x10, 1, (uint8_t)(8));
+    flash.write(0x18, 1, (uint8_t)(8));
+    printf("====== WROTE: 32 at 0x00, 16 at 0x04, 8 at 0x06, 8 at 0x07 ======\n\r");
+  
   }
 
   auto loop() -> void {
@@ -52,16 +62,32 @@ namespace mrover {
       }
         */
       if (button_state && count < 256) {
-        flash.write_float(curr_addr, (pi + count));
 
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
         HAL_Delay(100);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
         
-        read_float = flash.read_float(curr_addr);
-        printf("Read Float: %f\n\r", read_float);
-        ++count;
-        curr_addr += 8;
+        curr_addr = 0x00;
+        uint32_t read_data_32 = flash.read<uint32_t>(curr_addr);
+        printf("Read at address 0x00: %lu\n\r", read_data_32);
+        curr_addr = 0x04;
+        uint16_t read_data_16 = flash.read<uint16_t>(curr_addr);
+        printf("Read at address 0x04: %u\n\r", read_data_16);
+        curr_addr = 0x06;
+        uint8_t read_data_8 = flash.read<uint8_t>(curr_addr);
+        printf("Read at address 0x06: %u\n\r", read_data_8);
+        curr_addr = 0x07;
+        read_data_8 = flash.read<uint8_t>(curr_addr);
+        printf("Read at address 0x07: %u\n\r", read_data_8);
+        read_data_16 = flash.read<uint16_t>(0x08);
+        printf("Read at address 0x08: %u\n\r", read_data_16);
+        read_data_8 = flash.read<uint8_t>(0x10);
+        printf("Read at address 0x10: %u\n\r", read_data_8);
+        read_data_8 = flash.read<uint8_t>(0x18);
+        printf("Read at address 0x18: %u\n\r", read_data_8);
+        printf("====BYTE READING TEST====\n\r");
+        read_data_8 = flash.read_byte(start_addr + 0x10);
+        printf("Read at byte at address 0x10: %u\n\r", read_data_8);
 
       }
       
