68493d0231f396def60832a272000fbe276692a9
Eric D'Urso <edurso@edurso.com>
2025-11-30 15:41:44 -0500

    throw in some python infra

--- tools/scripts/can_header_gen.py
+++ tools/scripts/can_header_gen.py
@@ -1,6 +1,9 @@
-import dbc_parser
 import argparse
+from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
+
+from esw.dbc.parser import parse_file
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse dbc files to generate C header files.")
@@ -10,16 +13,30 @@ if __name__ == "__main__":
         type=str,
         help="List of dbc files"
     )
+    parser.add_argument(
+        "--dest",
+        "-d",
+        type=Path,
+        required=True,
+        help="Path to generated header file directory",
+    )
+    parser.add_argument(
+        "--ctx",
+        "-c",
+        type=Path,
+        required=True,
+        help="Path DBC build context",
+    )
     args = parser.parse_args()
 
-    env = Environment(loader=FileSystemLoader("."))
+    env = Environment(loader=FileSystemLoader(args.ctx))
     template = env.get_template("templates/dbc_header.h.j2")
 
     libs = ["cstdlib", "cstdint", "bit"]
 
     for f in args.files:
-        # Call dbc_parser.py to parse dbc file
-        dbc = dbc_parser.parse_file(f)
+        # Call parser.py to parse dbc file
+        dbc = parse_file(f)
 
         # Declare data object to pass into jinja2 template
         data = {
@@ -32,5 +49,8 @@ if __name__ == "__main__":
         rendered = template.render(data)
 
         # Write to new header file
-        with open(f"gen/{dbc.name}.h", "w") as f:
-            f.write(rendered)
+        if not args.dest.exists():
+            args.dest.mkdir(parents=True, exist_ok=True)
+        header_file = args.dest / f"{dbc.name}.h"
+        with open(header_file, "w") as handle:
+            handle.write(rendered)
