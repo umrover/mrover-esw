d19a6209d2410b9490137ddefa7c5b26e3753c44
nbaskins <nbaskins@umich.edu>
2026-01-11 15:35:38 -0500

    thp sensor works

--- src/thp-sensor/Core/Src/science.cpp
+++ src/thp-sensor/Core/Src/science.cpp
@@ -1,27 +1,32 @@
 #include "main.h"
 #include "thp_sensor.hpp"
 
-extern I2C_HandleTypeDef hi2c1;
+extern I2C_HandleTypeDef hi2c3;
+
+namespace mrover {
 
 THP thp_sensor;
 
 void event_loop() {
-	thp_sensor.read_thp();
-	HAL_Delay(100);
+	while (true) {
+		thp_sensor.read_thp();
+		HAL_Delay(100);
+	}
 }
 
 void init() {
-	thp_sensor = THP(&hi2c1);
+	thp_sensor = THP(&hi2c3);
 	thp_sensor.init();
 	event_loop();
+}
 }
 
 extern "C" {
 	void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-		thp_sensor.update_thp();
+		mrover::thp_sensor.update_thp();
 	}
 
 	void HAL_PostInit() {
-		init();
+		mrover::init();
 	}
 }
