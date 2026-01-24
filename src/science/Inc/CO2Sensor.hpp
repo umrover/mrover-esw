154fdfc3df0a6a254039dda2e1870881ad5d71e9
nbaskins <nbaskins@umich.edu>
2026-01-24 12:43:05 -0500

    added co2 sensor, getting nacks when writing to sensor, likely hardware issue?

---
+++ src/science/Inc/CO2Sensor.hpp
@@ -0,0 +1,51 @@
+#include "main.h"
+
+#define CO2_ADDR 0x29
+
+namespace mrover {
+	class CO2Sensor {
+	private:
+		I2C_HandleTypeDef* i2c; // i2c handle pointer
+        uint8_t rx_buf[2];
+		double ppm; // ozone value in ppm
+
+	public:
+		CO2Sensor() = default;
+
+		CO2Sensor (I2C_HandleTypeDef* i2c_in)
+			: i2c(i2c_in), ppm(0.0) {}
+
+		// returns the current ozone data in ppm
+		double get_co2() {
+			return ppm;
+		}
+
+        // updates ppm using rx_buf data -> conversion formula: ppm = ((rx_buf - 2^14) / 2^15) * 100
+        void update_co2() {
+            uint16_t raw = (rx_buf[0] << 8) | rx_buf[1];
+            ppm = ((raw - (1 << 14)) / (1 << 15)) * 100.00;
+        }
+
+        // requests raw co2 data over i2c, when sensor responds with data callback will be hit and will call receive_buf
+		void request_co2() {
+            uint8_t tx_buf[2] = {0x36, 0x39};
+			HAL_I2C_Master_Transmit_IT(i2c, CO2_ADDR << 1, tx_buf, 2);
+		}
+
+		// receives raw ozone data over i2c
+		void receive_buf() {
+			HAL_I2C_Master_Receive_IT(i2c, (CO2_ADDR << 1) | 1, rx_buf, 1);
+		}
+
+		// initializes the sensor to be in AUTO mode (sensor constantly sends data)
+		void init() {
+            // Disable CRC (0x3768)
+			uint8_t tx_buf1[2] = {0x37, 0x68};
+            HAL_I2C_Master_Transmit(i2c, CO2_ADDR << 1, tx_buf1, 2, HAL_MAX_DELAY);
+
+            // Set measurement mode -> standard measurement mode with 0-100% concentration in air
+			uint8_t tx_buf2[4] = {0x36, 0x15, 0x00, 0x13};
+            HAL_I2C_Master_Transmit(i2c, CO2_ADDR << 1, tx_buf2, 4, HAL_MAX_DELAY);
+		}
+	};
+}
--- src/science/Src/science.cpp
+++ src/science/Src/science.cpp
@@ -1,33 +1,35 @@
 #include "main.h"
+#include "CO2Sensor.hpp"
 #include <logger.hpp>
 #include <config.hpp>
 
 extern UART_HandleTypeDef hlpuart1;
+extern I2C_HandleTypeDef hi2c3;
 
 namespace mrover {
     static constexpr UART_HandleTypeDef* LPUART = &hlpuart1;
+    I2C_HandleTypeDef* i2c = &hi2c3;
 
     UART lpuart;
-
-    void event_loop() {
-        while (true) {}
-    }
+    CO2Sensor co2_sensor;
+    double co2 = 0;
 
     void init() {
-        // set debug led 1 high
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-
         lpuart = UART{LPUART, get_uart_options()};
 
         Logger::init(&lpuart);
         auto const& logger = Logger::instance();
+
+        co2_sensor = CO2Sensor(i2c);
+        co2_sensor.init();
+
         logger.info("Entering event loop...");
 
         while (true) {
-            logger.info("looping...");
+            HAL_Delay(2000);
+            co2_sensor.request_co2();
+            logger.info("CO2 ppm: %f", co2);
         }
-
-        event_loop();    
     }
 }
 
@@ -36,11 +38,12 @@ extern "C" {
         mrover::init();
     }
 
-    // void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
+    void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef* hi2c) {
+        mrover::co2_sensor.receive_buf();
+    }
 
-    // }
-
-    // void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-        
-    // }
+    void HAL_I2C_MasterRxCpltCallback (I2C_HandleTypeDef* hi2c) {
+        mrover::co2_sensor.update_co2();
+        mrover::co2 = mrover::co2_sensor.get_co2();
+    }
 }
