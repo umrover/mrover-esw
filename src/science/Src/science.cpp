5f0abb4d521535e0c3f670d3ab2af6a7834cbdea
nbaskins <nbaskins@umich.edu>
2026-01-24 13:08:23 -0500

    thp sensor works, seems like it might be a hardware issue with the co2 sensor :/

--- src/science/Src/science.cpp
+++ src/science/Src/science.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "CO2Sensor.hpp"
+#include "thp_sensor.hpp"
 #include <logger.hpp>
 #include <config.hpp>
 
@@ -11,8 +12,10 @@ namespace mrover {
     I2C_HandleTypeDef* i2c = &hi2c3;
 
     UART lpuart;
-    CO2Sensor co2_sensor;
-    double co2 = 0;
+    THP thp_sensor;
+    // CO2Sensor co2_sensor;
+    THP_data thp_data;
+    // double co2 = 0;
 
     void init() {
         lpuart = UART{LPUART, get_uart_options()};
@@ -20,15 +23,21 @@ namespace mrover {
         Logger::init(&lpuart);
         auto const& logger = Logger::instance();
 
-        co2_sensor = CO2Sensor(i2c);
-        co2_sensor.init();
+        thp_sensor = THP(&hi2c3);
+	    thp_sensor.init();
+    
+        // co2_sensor = CO2Sensor(i2c);
+        // co2_sensor.init();
 
         logger.info("Entering event loop...");
 
         while (true) {
-            HAL_Delay(2000);
-            co2_sensor.request_co2();
-            logger.info("CO2 ppm: %f", co2);
+            HAL_Delay(1000);
+            thp_sensor.read_thp();
+            // co2_sensor.request_co2();
+            logger.info("temp: %f", thp_data.temp);
+            logger.info("humidity: %f", thp_data.humidity);
+            logger.info("pressure: %f", thp_data.pressure);
         }
     }
 }
@@ -39,11 +48,16 @@ extern "C" {
     }
 
     void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef* hi2c) {
-        mrover::co2_sensor.receive_buf();
+        // mrover::co2_sensor.receive_buf();
     }
 
     void HAL_I2C_MasterRxCpltCallback (I2C_HandleTypeDef* hi2c) {
-        mrover::co2_sensor.update_co2();
-        mrover::co2 = mrover::co2_sensor.get_co2();
+        // mrover::co2_sensor.update_co2();
+        // mrover::co2 = mrover::co2_sensor.get_co2();
     }
+
+    void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+		mrover::thp_sensor.update_thp();
+        mrover::thp_data = mrover::thp_sensor.get_thp();
+	}
 }
