6ef0c42b52c8c1e6690beb6cd58e04d501ddd805
nbaskins <nbaskins@umich.edu>
2026-01-28 16:41:11 -0500

    able to receive values but percentage is sketchy

--- src/science/Src/science.cpp
+++ src/science/Src/science.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "CO2Sensor.hpp"
+#include "stm32g4xx_hal.h"
 #include "thp_sensor.hpp"
 #include <logger.hpp>
 #include <config.hpp>
@@ -9,12 +10,12 @@ extern I2C_HandleTypeDef hi2c3;
 
 namespace mrover {
     static constexpr UART_HandleTypeDef* LPUART = &hlpuart1;
-    I2C_HandleTypeDef* i2c = &hi2c3;
+    static constexpr I2C_HandleTypeDef* I2C = &hi2c3;
 
     UART lpuart;
-    THP thp_sensor;
+    // THP thp_sensor;
     CO2Sensor co2_sensor;
-    THP_data thp_data;
+    // THP_data thp_data;
     double co2 = 0;
 
     void init() {
@@ -23,23 +24,23 @@ namespace mrover {
         Logger::init(&lpuart);
         auto const& logger = Logger::instance();
 
-        thp_sensor = THP(&hi2c3);
-	    thp_sensor.init();
+        // thp_sensor = THP(&hi2c3);
+	    // thp_sensor.init();
     
-        co2_sensor = CO2Sensor(i2c);
+        co2_sensor = CO2Sensor(I2C);
         co2_sensor.init();
 
-        logger.info("Entering event loop...");
+        logger.info("Polling sensor...");
+
+        co2_sensor.request_co2();
 
         while (true) {
-            HAL_Delay(1000);
-            thp_sensor.read_thp();
-            logger.info("temp: %f", thp_data.temp);
-            logger.info("humidity: %f", thp_data.humidity);
-            logger.info("pressure: %f", thp_data.pressure);
+            // HAL_Delay(1000);
+            // thp_sensor.read_thp();
+            // logger.info("temp: %f", thp_data.temp);
+            // logger.info("humidity: %f", thp_data.humidity);
+            // logger.info("pressure: %f", thp_data.pressure);
 
-            HAL_Delay(1000);
-            co2_sensor.request_co2();
             logger.info("co2: %f", co2);
         }
     }
@@ -51,16 +52,18 @@ extern "C" {
     }
 
     void HAL_I2C_MasterTxCpltCallback (I2C_HandleTypeDef* hi2c) {
+        for (int i = 0; i < 1000000; i++);
         mrover::co2_sensor.receive_buf();
     }
 
     void HAL_I2C_MasterRxCpltCallback (I2C_HandleTypeDef* hi2c) {
         mrover::co2_sensor.update_co2();
         mrover::co2 = mrover::co2_sensor.get_co2();
+        mrover::co2_sensor.request_co2();
     }
 
-    void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-		mrover::thp_sensor.update_thp();
-        mrover::thp_data = mrover::thp_sensor.get_thp();
-	}
+    // void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+	// 	mrover::thp_sensor.update_thp();
+    //     mrover::thp_data = mrover::thp_sensor.get_thp();
+	// }
 }
