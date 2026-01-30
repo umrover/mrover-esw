db284eb978dea5df36ae67f089efdf4da57cec39
crazab0i <=>
2026-01-30 18:19:01 -0500

    format

--- src/canalyzer/can_code/tests/test.cpp
+++ src/canalyzer/can_code/tests/test.cpp
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
 #include <linux/can.h>
 #include <linux/can/raw.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <string.h>
 #include <string>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <thread>
+#include <unistd.h>
 #include <vector>
 
-int helper(const std::string &can_name);
+int helper(std::string const& can_name);
 
 int main() {
     std::vector<std::thread> threads;
@@ -25,7 +25,7 @@ int main() {
     return 0;
 }
 
-int helper(const std::string &can_name) {
+int helper(std::string const& can_name) {
     for (int i = 0; i < 100; ++i) {
         int s;
         struct sockaddr_can addr;
@@ -49,7 +49,7 @@ int helper(const std::string &can_name) {
         addr.can_family = AF_CAN;
         addr.can_ifindex = ifr.ifr_ifindex;
 
-        if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        if (bind(s, (struct sockaddr*) &addr, sizeof(addr)) < 0) {
             perror("Bind");
             return 1;
         }
@@ -57,22 +57,22 @@ int helper(const std::string &can_name) {
         // Enable CAN FD support on socket
         int enable_canfd = 1;
         if (setsockopt(s, SOL_CAN_RAW, CAN_RAW_FD_FRAMES,
-                    &enable_canfd, sizeof(enable_canfd)) != 0) {
+                       &enable_canfd, sizeof(enable_canfd)) != 0) {
             perror("setsockopt CAN_RAW_FD_FRAMES");
             return 1;
         }
-        
-            
+
+
         // Prepare CAN FD frame
         frame.can_id = 0x123;
-        frame.len = 16;   // CAN FD supports up to 64
-        frame.flags = 0;  // no BRS, no ESI
+        frame.len = 16;  // CAN FD supports up to 64
+        frame.flags = 0; // no BRS, no ESI
         for (int i = 0; i < frame.len; i++) {
             frame.data[i] = i;
         }
 
         // Send CAN FD frame
-        
+
         ssize_t nbytes = write(s, &frame, sizeof(frame));
         if (nbytes < 0) {
             perror("Write");
@@ -81,7 +81,6 @@ int helper(const std::string &can_name) {
             fprintf(stderr, "Partial CAN frame write: %zd bytes\n", nbytes);
             return 1;
         }
-        
     }
     return 0;
 }
--- src/canalyzer/can_code/tests/test_factory.cpp
+++ src/canalyzer/can_code/tests/test_factory.cpp
@@ -4,40 +4,40 @@
 
 namespace logger {
 
-void test_factory(std::vector<Logger> &loggers) {
-    assert(loggers[0].can_bus_name == "vcan0");
-    assert(loggers[0].log_ids.count(100));
-    assert(loggers[0].log_ids.count(101));
-    assert(loggers[0].log_all == false);
-    assert(loggers[0].ascii_log_file_path == "./log/can0.log");
-    assert(loggers[0].dbc_file_paths.count("./dbc/can0.dbc"));
+    void test_factory(std::vector<Logger>& loggers) {
+        assert(loggers[0].can_bus_name == "vcan0");
+        assert(loggers[0].log_ids.count(100));
+        assert(loggers[0].log_ids.count(101));
+        assert(loggers[0].log_all == false);
+        assert(loggers[0].ascii_log_file_path == "./log/can0.log");
+        assert(loggers[0].dbc_file_paths.count("./dbc/can0.dbc"));
 
-    assert(loggers[1].can_bus_name == "vcan1");
-    assert(loggers[1].log_ids.count(1));
-    assert(loggers[1].log_ids.count(2));
-    assert(loggers[1].log_ids.count(3));
-    assert(loggers[1].log_all == false);
-    assert(loggers[1].ascii_log_file_path == "./log/can1.log");
-    assert(loggers[1].dbc_file_paths.count("./dbc/can1.dbc"));
+        assert(loggers[1].can_bus_name == "vcan1");
+        assert(loggers[1].log_ids.count(1));
+        assert(loggers[1].log_ids.count(2));
+        assert(loggers[1].log_ids.count(3));
+        assert(loggers[1].log_all == false);
+        assert(loggers[1].ascii_log_file_path == "./log/can1.log");
+        assert(loggers[1].dbc_file_paths.count("./dbc/can1.dbc"));
 
-    assert(loggers[2].can_bus_name == "vcan2");
-    assert(loggers[2].log_ids.count(98));
-    assert(loggers[2].log_ids.count(123));
-    assert(loggers[2].log_all == false);
-    assert(loggers[2].ascii_log_file_path == "./log/can2.log");
-    assert(loggers[2].dbc_file_paths.count("./dbc/can2.dbc"));
+        assert(loggers[2].can_bus_name == "vcan2");
+        assert(loggers[2].log_ids.count(98));
+        assert(loggers[2].log_ids.count(123));
+        assert(loggers[2].log_all == false);
+        assert(loggers[2].ascii_log_file_path == "./log/can2.log");
+        assert(loggers[2].dbc_file_paths.count("./dbc/can2.dbc"));
 
-    assert(loggers[3].can_bus_name == "vcan3");
-    assert(loggers[3].log_ids.count(21));
-    assert(loggers[3].log_ids.count(2010339));
-    assert(loggers[3].log_all == false);
-    assert(loggers[3].dbc_file_paths.count("./dbc/can3.dbc"));
-    assert(loggers[3].dbc_file_paths.count("./dbc/can4.dbc"));
-    assert(loggers[3].ascii_log_file_path == "./log/can3.log");
+        assert(loggers[3].can_bus_name == "vcan3");
+        assert(loggers[3].log_ids.count(21));
+        assert(loggers[3].log_ids.count(2010339));
+        assert(loggers[3].log_all == false);
+        assert(loggers[3].dbc_file_paths.count("./dbc/can3.dbc"));
+        assert(loggers[3].dbc_file_paths.count("./dbc/can4.dbc"));
+        assert(loggers[3].ascii_log_file_path == "./log/can3.log");
 
-    std::cout << "Test Passing!" << std::endl;
-}
-}
+        std::cout << "Test Passing!" << std::endl;
+    }
+} // namespace logger
 
 int main() {
     std::string file_name = "../logger_start.yaml";
