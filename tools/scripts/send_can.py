46f8a94d4119c08d40a8f5d637e17aed9663fcf7
Eric D'Urso <edurso@edurso.com>
2026-03-17 19:45:25 -0400

    joint b

--- tools/scripts/send_can.py
+++ tools/scripts/send_can.py
@@ -6,10 +6,9 @@ from esw.can.dbc import get_dbc
 from esw.visualization.async_plotter import AsyncPlotter
 
 if __name__ == "__main__":
-    NUM_LOOPS = 50
     LOOP_DELAY = 0.05
-    TARGET = -1.0
-    CAN_ID = 54
+    TARGET = -0.004
+    CAN_ID = 49
     SRC_ID = 16
 
     with AsyncPlotter(max_size=200, loop_delay=LOOP_DELAY, x_label="Time (s)", y_label="Waveform") as plotter:
@@ -23,14 +22,13 @@ if __name__ == "__main__":
 
         with CANBus(get_dbc(dbc_name="MRoverCAN"), "can2", on_recv=on_msg_recv) as bus:
             # set mode to velocity
-            # for _ in range(5):
-            #     bus.send("BMCModeCmd", {"mode": 7, "enable": 1}, src_id=SRC_ID, dest_id=CAN_ID)
+            for _ in range(5):
+                bus.send("BMCModeCmd", {"mode": 7, "enable": 1}, src_id=SRC_ID, dest_id=CAN_ID)
 
             # set mode to throttle
-            for _ in range(5):
-                bus.send("BMCModeCmd", {"mode": 5, "enable": 1}, src_id=SRC_ID, dest_id=CAN_ID)
+            # for _ in range(5):
+            #     bus.send("BMCModeCmd", {"mode": 5, "enable": 1}, src_id=SRC_ID, dest_id=CAN_ID)
 
             while True:
-                for _ in range(NUM_LOOPS):
-                    bus.send("BMCTargetCmd", {"target": TARGET, "target_valid": 1}, src_id=SRC_ID, dest_id=CAN_ID)
-                    sleep(LOOP_DELAY)
+                bus.send("BMCTargetCmd", {"target": TARGET, "target_valid": 1}, src_id=SRC_ID, dest_id=CAN_ID)
+                sleep(LOOP_DELAY)
