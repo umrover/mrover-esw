cdb0b1552e3748c5d0ba71fb8e667e6a71249f86
crazab0i <mattsasato@outlook.com>
2026-04-12 15:13:59 -0400

    devboard start

---
+++ src/devboard/Src/test.cpp
@@ -0,0 +1,14 @@
+#include "test.h"
+#include "stm32g431xx.h"
+#include "stm32g4xx_hal.h"
+
+void LED_TEST() {
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+    HAL_Delay(1000);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+    HAL_Delay(1000);
+} 
