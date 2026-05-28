f72cb92fc0047fc9c0cbd04530d263b996769b57
Eric D'Urso <edurso@edurso.com>
2026-05-27 20:27:09 -0600

    format

--- tools/setup_venv.py
+++ tools/setup_venv.py
@@ -9,9 +9,9 @@ venv_dir = sys.argv[1]
 pyproject_toml = sys.argv[2]
 venv_pip = sys.argv[3]
 
-pyvenv_cfg = os.path.join(venv_dir, 'pyvenv.cfg')
+pyvenv_cfg = os.path.join(venv_dir, "pyvenv.cfg")
 venv_exists = os.path.exists(pyvenv_cfg)
-timestamp_file = os.path.join(venv_dir, 'package.timestamp')
+timestamp_file = os.path.join(venv_dir, "package.timestamp")
 
 if venv_exists and os.path.exists(timestamp_file):
     if os.path.getmtime(timestamp_file) >= os.path.getmtime(pyproject_toml):
@@ -20,7 +20,7 @@ if venv_exists and os.path.exists(timestamp_file):
 
 has_internet = False
 try:
-    urllib.request.urlopen('https://pypi.org', timeout=3)
+    urllib.request.urlopen("https://pypi.org", timeout=3)
     has_internet = True
 except Exception:
     pass
@@ -28,10 +28,17 @@ except Exception:
 if has_internet:
     try:
         if not venv_exists:
-            subprocess.run([sys.executable, '-m', 'venv', venv_dir], check=True, capture_output=True)
+            subprocess.run([sys.executable, "-m", "venv", venv_dir], check=True, capture_output=True)
 
-        subprocess.run([venv_pip, 'install', '--upgrade', 'pip', 'setuptools', 'wheel'], check=True, capture_output=True)
-        subprocess.run([venv_pip, 'install', '--editable', '.[dev]'], cwd=os.path.dirname(pyproject_toml), check=True, capture_output=True)
+        subprocess.run(
+            [venv_pip, "install", "--upgrade", "pip", "setuptools", "wheel"], check=True, capture_output=True
+        )
+        subprocess.run(
+            [venv_pip, "install", "--editable", ".[dev]"],
+            cwd=os.path.dirname(pyproject_toml),
+            check=True,
+            capture_output=True,
+        )
 
         pathlib.Path(timestamp_file).touch()
 
@@ -46,7 +53,7 @@ else:
         sys.exit(1)
 
     try:
-        result = subprocess.run([venv_pip, 'list', '--outdated'], capture_output=True, text=True, timeout=4)
+        result = subprocess.run([venv_pip, "list", "--outdated"], capture_output=True, text=True, timeout=4)
         pathlib.Path(timestamp_file).touch()
 
         if result.returncode == 0 and result.stdout.strip():
